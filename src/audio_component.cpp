11dbf963847488e4e32c5bf938b31df3b8acb174
Samuel Grayson <sam@samgrayson.me>
2020-05-27 11:07:11 -0500

    Updated switchboard interface

--- src/audio_component.cpp
+++ src/audio_component.cpp
@@ -20,7 +20,7 @@ class audio_component : public threadloop
 public:
 	audio_component(phonebook *pb)
 		: sb{pb->lookup_impl<switchboard>()},
-		_m_pose{sb->subscribe_latest<pose_type>("slow_pose")}
+		_m_pose{sb->get_reader<pose_type>("slow_pose")}
 	{
 		ILLIXR_AUDIO::ABAudio::ProcessType processDecode(ILLIXR_AUDIO::ABAudio::ProcessType::DECODE);	
 		decoder = new ILLIXR_AUDIO::ABAudio("", processDecode);
@@ -50,7 +50,7 @@ public:
 		sync += std::chrono::microseconds(num_epoch*((int)(AUDIO_EPOCH*1000000))); 
 
 		logger->log_start(std::chrono::high_resolution_clock::now());
-		auto volatile most_recent_pose = _m_pose->get_latest_ro();
+		auto volatile most_recent_pose = _m_pose.get_latest_ro();
 		encoder->processBlock();
 		decoder->processBlock();
 		logger->log_end(std::chrono::high_resolution_clock::now());
@@ -59,7 +59,7 @@ public:
 private:
 	switchboard *sb;
 	start_end_logger* logger;
-	std::unique_ptr<reader_latest<pose_type>> _m_pose;
+	switchboard::reader<pose_type> _m_pose;
 	ILLIXR_AUDIO::ABAudio* decoder, *encoder;
 	std::chrono::time_point<std::chrono::system_clock> sync;
 };
