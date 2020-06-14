17b99e057f72e6311df867c00cc64ce07021dfa2
Samuel Grayson <sam@samgrayson.me>
2020-06-14 02:05:53 -0500

    Get CPU time

--- src/audio_component.cpp
+++ src/audio_component.cpp
@@ -18,24 +18,14 @@ using namespace ILLIXR;
 class audio_component : public threadloop
 {
 public:
-	audio_component(phonebook *pb)
-		: sb{pb->lookup_impl<switchboard>()},
-		_m_pose{sb->subscribe_latest<pose_type>("slow_pose")}
-	{
-		ILLIXR_AUDIO::ABAudio::ProcessType processDecode(ILLIXR_AUDIO::ABAudio::ProcessType::DECODE);	
-		decoder = new ILLIXR_AUDIO::ABAudio("", processDecode);
-		ILLIXR_AUDIO::ABAudio::ProcessType processEncode(ILLIXR_AUDIO::ABAudio::ProcessType::ENCODE);	
-		encoder = new ILLIXR_AUDIO::ABAudio("", processEncode);
-
-		decoder->loadSource();
-		encoder->loadSource();
-
-		logger = new start_end_logger("audio");
-	}
-	~audio_component(){
-		delete encoder;
-		delete decoder;
-	}
+	audio_component(std::string name_, phonebook *pb_)
+		: threadloop{name_, pb_}
+		, sb{pb->lookup_impl<switchboard>()}
+		, _m_pose{sb->subscribe_latest<pose_type>("slow_pose")}
+		, decoder{"", ILLIXR_AUDIO::ABAudio::ProcessType::DECODE}
+		, encoder{"", ILLIXR_AUDIO::ABAudio::ProcessType::ENCODE}
+		, logger{"audio"}
+	{ }
 
 	virtual void _p_one_iteration(){
 		std::chrono::time_point<std::chrono::system_clock> blockStart = std::chrono::high_resolution_clock::now();
@@ -49,18 +39,18 @@ public:
 		int num_epoch = ceil(timespent/AUDIO_EPOCH);
 		sync += std::chrono::microseconds(num_epoch*((int)(AUDIO_EPOCH*1000000))); 
 
-		logger->log_start(std::chrono::high_resolution_clock::now());
+		logger.log_start(std::chrono::high_resolution_clock::now());
 		auto volatile most_recent_pose = _m_pose->get_latest_ro();
-		encoder->processBlock();
-		decoder->processBlock();
-		logger->log_end(std::chrono::high_resolution_clock::now());
+		encoder.processBlock();
+		decoder.processBlock();
+		logger.log_end(std::chrono::high_resolution_clock::now());
 	}
 
 private:
 	switchboard *sb;
-	start_end_logger* logger;
+	start_end_logger logger;
 	std::unique_ptr<reader_latest<pose_type>> _m_pose;
-	ILLIXR_AUDIO::ABAudio* decoder, *encoder;
+	ILLIXR_AUDIO::ABAudio decoder, encoder;
 	std::chrono::time_point<std::chrono::system_clock> sync;
 };
 
