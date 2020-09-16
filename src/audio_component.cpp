a996062a3543bf91a0be1956c83684668553d7c8
Samuel Grayson <sam@samgrayson.me>
2020-09-16 13:30:03 -0500

    Made encoding and decoding threads of audio_pipeline comp

--- src/audio_component.cpp
+++ src/audio_component.cpp
@@ -46,4 +46,60 @@ private:
 	std::chrono::high_resolution_clock::time_point last_iteration;
 };
 
-PLUGIN_MAIN(audio_encoding)
+class audio_decoding : public threadloop
+{
+public:
+	audio_decoding(std::string name_, phonebook *pb_)
+		: threadloop{name_, pb_}
+		, sb{pb->lookup_impl<switchboard>()}
+		, _m_pose{sb->subscribe_latest<pose_type>("slow_pose")}
+		, decoder{"", ILLIXR_AUDIO::ABAudio::ProcessType::DECODE}
+		, last_iteration{std::chrono::high_resolution_clock::now()}
+	{
+		decoder.loadSource();
+	}
+
+	virtual skip_option _p_should_skip() override {
+		last_iteration += std::chrono::milliseconds{21};
+		std::this_thread::sleep_for(last_iteration - std::chrono::high_resolution_clock::now());
+		return skip_option::run;
+	}
+
+	virtual void _p_one_iteration() override {
+		[[maybe_unused]] auto most_recent_pose = _m_pose->get_latest_ro();
+		decoder.processBlock();
+	}
+
+private:
+	const std::shared_ptr<switchboard> sb;
+	std::unique_ptr<reader_latest<pose_type>> _m_pose;
+	ILLIXR_AUDIO::ABAudio decoder;
+	std::chrono::high_resolution_clock::time_point last_iteration;
+};
+
+class audio_pipeline : public plugin {
+public:
+	audio_pipeline(std::string name_, phonebook *pb_)
+		: plugin{name_, pb_}
+		, audio_encoding_{"audio_encoding", pb_}
+		, audio_decoding_{"audio_decoding", pb_}
+	{ }
+
+	virtual void start() override {
+		audio_encoding_.start();
+		audio_decoding_.start();
+		plugin::start();
+	}
+
+    virtual void stop() override {
+		audio_encoding_.stop();
+		audio_decoding_.stop();
+        plugin::stop();
+    }
+
+private:
+	audio_encoding audio_encoding_;
+	audio_decoding audio_decoding_;
+};
+
+PLUGIN_MAIN(audio_pipeline)
