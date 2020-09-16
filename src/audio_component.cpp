c45a9af935ee5d1ee24e8218fa1063a89766ebf5
Samuel Grayson <sam@samgrayson.me>
2020-09-16 15:29:56 -0500

    Reduce code duplication

--- src/audio_component.cpp
+++ src/audio_component.cpp
@@ -15,17 +15,18 @@ using namespace ILLIXR;
 
 #define AUDIO_EPOCH (1024.0/48000.0)
 
-class audio_encoding : public threadloop
+class audio_xcoding : public threadloop
 {
 public:
-	audio_encoding(std::string name_, phonebook *pb_)
-		: threadloop{name_, pb_}
+	audio_xcoding(phonebook *pb_, bool encoding)
+		: threadloop{encoding ? "audio_encoding" : "audio_decoding", pb_}
 		, sb{pb->lookup_impl<switchboard>()}
 		, _m_pose{sb->subscribe_latest<pose_type>("slow_pose")}
-		, encoder{"", ILLIXR_AUDIO::ABAudio::ProcessType::ENCODE}
+		, xcoder{"", encoding ? ILLIXR_AUDIO::ABAudio::ProcessType::ENCODE : ILLIXR_AUDIO::ABAudio::ProcessType::DECODE}
 		, last_iteration{std::chrono::high_resolution_clock::now()}
+		, encoding_{encoding}
 	{
-		encoder.loadSource();
+		xcoder.loadSource();
 	}
 
 	virtual skip_option _p_should_skip() override {
@@ -35,71 +36,43 @@ public:
 	}
 
 	virtual void _p_one_iteration() override {
-		[[maybe_unused]] auto most_recent_pose = _m_pose->get_latest_ro();
-		encoder.processBlock();
+		if (!encoding_) {
+			[[maybe_unused]] auto most_recent_pose = _m_pose->get_latest_ro();
+		}
+		xcoder.processBlock();
 	}
 
 private:
 	const std::shared_ptr<switchboard> sb;
 	std::unique_ptr<reader_latest<pose_type>> _m_pose;
-	ILLIXR_AUDIO::ABAudio encoder;
+	ILLIXR_AUDIO::ABAudio xcoder;
 	std::chrono::high_resolution_clock::time_point last_iteration;
-};
-
-class audio_decoding : public threadloop
-{
-public:
-	audio_decoding(std::string name_, phonebook *pb_)
-		: threadloop{name_, pb_}
-		, sb{pb->lookup_impl<switchboard>()}
-		, _m_pose{sb->subscribe_latest<pose_type>("slow_pose")}
-		, decoder{"", ILLIXR_AUDIO::ABAudio::ProcessType::DECODE}
-		, last_iteration{std::chrono::high_resolution_clock::now()}
-	{
-		decoder.loadSource();
-	}
-
-	virtual skip_option _p_should_skip() override {
-		last_iteration += std::chrono::milliseconds{21};
-		std::this_thread::sleep_for(last_iteration - std::chrono::high_resolution_clock::now());
-		return skip_option::run;
-	}
-
-	virtual void _p_one_iteration() override {
-		[[maybe_unused]] auto most_recent_pose = _m_pose->get_latest_ro();
-		decoder.processBlock();
-	}
-
-private:
-	const std::shared_ptr<switchboard> sb;
-	std::unique_ptr<reader_latest<pose_type>> _m_pose;
-	ILLIXR_AUDIO::ABAudio decoder;
-	std::chrono::high_resolution_clock::time_point last_iteration;
+	bool encoding_;
 };
 
 class audio_pipeline : public plugin {
 public:
 	audio_pipeline(std::string name_, phonebook *pb_)
 		: plugin{name_, pb_}
-		, audio_encoding_{"audio_encoding", pb_}
-		, audio_decoding_{"audio_decoding", pb_}
+		, audio_encoding{pb_, true }
+		, audio_decoding{pb_, false}
 	{ }
 
 	virtual void start() override {
-		audio_encoding_.start();
-		audio_decoding_.start();
+		audio_encoding.start();
+		audio_decoding.start();
 		plugin::start();
 	}
 
     virtual void stop() override {
-		audio_encoding_.stop();
-		audio_decoding_.stop();
+		audio_encoding.stop();
+		audio_decoding.stop();
         plugin::stop();
     }
 
 private:
-	audio_encoding audio_encoding_;
-	audio_decoding audio_decoding_;
+	audio_xcoding audio_encoding;
+	audio_xcoding audio_decoding;
 };
 
 PLUGIN_MAIN(audio_pipeline)
