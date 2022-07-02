8519b744b1947bc0d62c85821c750cd3d6aa6f9d
Muhammad Huzaifa <mhuzai@users.noreply.github.com>
2022-07-01 21:25:05 -0500

    Fix plugin frequency, migrate to RelativeClock, and clean up code (#18)

--- src/audio_component.cpp
+++ src/audio_component.cpp
@@ -1,37 +1,34 @@
-#include <chrono>
 #include <future>
-#include <fstream>
 #include <thread>
 
-#include "common/threadloop.hpp"
-#include "common/switchboard.hpp"
 #include "common/data_format.hpp"
 #include "common/phonebook.hpp"
-#include "common/logger.hpp"
+#include "common/relative_clock.hpp"
+#include "common/switchboard.hpp"
+#include "common/threadloop.hpp"
 
 #include <audio.h>
 
 using namespace ILLIXR;
-
-#define AUDIO_EPOCH (1024.0/48000.0)
 
 class audio_xcoding : public threadloop
 {
 public:
     audio_xcoding(phonebook *pb_, bool encoding)
         : threadloop{encoding ? "audio_encoding" : "audio_decoding", pb_}
-        , sb{pb->lookup_impl<switchboard>()}
-        , _m_pose{sb->get_reader<pose_type>("slow_pose")}
+        , _m_sb{pb->lookup_impl<switchboard>()}
+        , _m_clock{pb->lookup_impl<RelativeClock>()}
+        , _m_pose{_m_sb->get_reader<pose_type>("slow_pose")}
         , xcoder{"", encoding ? ILLIXR_AUDIO::ABAudio::ProcessType::ENCODE : ILLIXR_AUDIO::ABAudio::ProcessType::DECODE}
-        , last_iteration{std::chrono::high_resolution_clock::now()}
+        , last_time{_m_clock->now()}
         , encoding_{encoding}
     {
         xcoder.loadSource();
     }
 
     virtual skip_option _p_should_skip() override {
-        last_iteration += std::chrono::milliseconds{21};
-        std::this_thread::sleep_for(last_iteration - std::chrono::high_resolution_clock::now());
+        last_time += audio_period;
+        std::this_thread::sleep_for(last_time - _m_clock->now());
         return skip_option::run;
     }
 
@@ -43,10 +40,12 @@ public:
     }
 
 private:
-    const std::shared_ptr<switchboard> sb;
+    const std::shared_ptr<switchboard> _m_sb;
+    const std::shared_ptr<RelativeClock> _m_clock;
     switchboard::reader<pose_type> _m_pose;
     ILLIXR_AUDIO::ABAudio xcoder;
-    std::chrono::high_resolution_clock::time_point last_iteration;
+    time_point last_time;
+    static constexpr duration audio_period{freq2period(static_cast<double>(SAMPLERATE) / static_cast<double>(BLOCK_SIZE))};
     bool encoding_;
 };
 
